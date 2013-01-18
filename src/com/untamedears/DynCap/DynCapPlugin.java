bc507181cbdf0a4d9b7929197145dcaeba2fded9
erocs <github@erocs.org>
2013-01-17 21:10:31 -0800

    Fix the bug - Use pull #2 and adjust the total player count on logout - Lock down the console commands via bukkit permissions

--- src/com/untamedears/DynCap/DynCapPlugin.java
+++ src/com/untamedears/DynCap/DynCapPlugin.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -18,7 +19,7 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 	private int dynamicPlayerCap = 1000;
 	private Logger log;
 	private boolean whiteListEnabled = false;
-	
+
 	public void onEnable() {
 		log = this.getLogger();
 		commands = new DynCapCommands(this, log);
@@ -27,60 +28,62 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 		for (String command : getDescription().getCommands().keySet()) {
 			getCommand(command).setExecutor(commands);
 		}
+
+		// Give the console permission
+		ConsoleCommandSender console = getServer().getConsoleSender();
+		console.addAttachment(this, "dyncap.console", true);
 	}
-	
-	public void onDisable() {
-		
-	}
-	
+
+	public void onDisable() {}
+
 	@EventHandler(priority=EventPriority.MONITOR)
 	public void onPlayerJoinEvent(PlayerJoinEvent event) {
-		updatePlayerCap();
+		updatePlayerCap(getPlayerCount());
 	}
-	
+
 	@EventHandler(priority=EventPriority.MONITOR)
 	public void onPlayerQuitEvent(PlayerQuitEvent event) {
-		updatePlayerCap();
+		updatePlayerCap(getPlayerCount() - 1);
 	}
-	
+
 	@EventHandler(priority=EventPriority.MONITOR)
 	public void onPlayerKickEvent(PlayerKickEvent event) {
-		updatePlayerCap();
+		updatePlayerCap(getPlayerCount() - 1);
 	}
-	
+
 	public void setPlayerCap(int cap) {
 		dynamicPlayerCap = cap;
-		updatePlayerCap();
+		updatePlayerCap(getPlayerCount());
 	}
-	
+
 	public int getPlayerCap() {
 		return dynamicPlayerCap;
 	}
-	
-	private void updatePlayerCap() {
-		int playerCount = getPlayerCount();
+
+	private void updatePlayerCap(int playerCount) {
 		if (playerCount >= dynamicPlayerCap) {
-			setWhitelist(true);
+			setWhitelist(true, playerCount);
 		} else if (playerCount < dynamicPlayerCap) {
-			setWhitelist(false);
+			setWhitelist(false, playerCount);
 		}
 	}
-	
-	private void setWhitelist(boolean enabled) {
-		Bukkit.setWhitelist(enabled);
-		Integer playerCount  = getPlayerCount();
-		Integer cap = getPlayerCap();
-		String message = playerCount.toString()+"/"+cap.toString()+" players online ";
-		if (enabled && !whiteListEnabled) {
-			message+=" dynamic cap enabled.";
-			log.info(message);
-		} else if (!enabled && whiteListEnabled) {
-			message+=" dynamic cap disabled.";
+
+	private void setWhitelist(boolean enabled, int playerCount) {
+		if ((enabled && !whiteListEnabled) || (!enabled && whiteListEnabled)) {
+			Integer cap = getPlayerCap();
+			String state_message = "disabled";
+			if (enabled) {
+				state_message = "enabled";
+			}
+			String message = String.format(
+					"%d/%d players online dynamic cap %s.",
+					playerCount, cap, state_message);
 			log.info(message);
 		}
 		whiteListEnabled = enabled;
+		Bukkit.setWhitelist(enabled);
 	}
-	
+
 	public int getPlayerCount() {
 		return this.getServer().getOnlinePlayers().length;
 	}
