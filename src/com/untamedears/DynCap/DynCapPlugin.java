44232a1c17429c606dfb301a106c1c97a1c57496
shadowjay1 <jrdn.ash@gmail.com>
2013-05-21 01:53:36 -0400

    Untested fix to not showing the actual player cap
    I am unable to test this at the moment, but unless I somehow missed a mistake, this should work.

--- src/com/untamedears/DynCap/DynCapPlugin.java
+++ src/com/untamedears/DynCap/DynCapPlugin.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -41,6 +42,11 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 	}
 
 	public void onDisable() {}
+	
+	@EventHandler
+	public void onServerListPingEvent(ServerListPingEvent event) {
+		event.setMaxPlayers(getPlayerCap());
+	}
 
 	/*@EventHandler(priority=EventPriority.MONITOR)
 	public void onPlayerJoinEvent(PlayerJoinEvent event) {
